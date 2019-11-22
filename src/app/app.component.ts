3bb4feef03ee133e096a963e053eb798da568fdb
Chris Faria <chrisfaria@gmail.com>
2019-11-22 08:01:44 -0500

    Connected to signalR and setup notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import * as signalR from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService]
 })
 export class AppComponent {
-  title = 'signalr-web-notification-client';
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    // Create a connection to the SignalR server
+    const connection = new signalR.HubConnectionBuilder()
+      .configureLogging(signalR.LogLevel.Information)
+      .withUrl("http://localhost:51000/notifyhub")
+      .build();
+
+    // Setup a function for the SignalR server to be able to call
+    connection.on("BroadcastMessage", (type: string, payload: string) => {
+      this.messageService.add({ severity: type, summary: 'SignalR Message', detail: payload });
+    });
+
+    // Start the connection to the SignalR server
+    connection.start().then(function () {
+      console.log('Connected to SignalR hub!');
+    }).catch(function (err) {
+      return console.error(err.toString());
+    });
+  }
+
+  handleClick() {
+    console.log('clicked');
+    this.messageService.add({ severity: 'success', summary: 'From button', detail: 'Hello from your friendly neighborhood Spiderman' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
